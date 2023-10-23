276886964c4ad56219dee8cf748c5bb1b1f25397
Christian Bongard <christian.bongard90@gmail.com>
2023-10-23 10:04:26 +0400

    added the home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "pages#home" # will open in / route
+  #  verb "path", to: "controller#action"
+  get "about", to: "pages#about"
 end
